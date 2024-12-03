33e63e89b1a9ba9f214348ebe8c72198f8ae7756
Marco Montalbano <marcomontalbano.work@gmail.com>
2024-12-03 07:39:57 +0100

    feat: complete day 3 - part 2

--- days/03/main.ts
+++ days/03/main.ts
@@ -1,7 +1,7 @@
 // Day 3: Mull It Over
 // https://adventofcode.com/2024/day/3
 
-export function parseProgram(str: string): [number, number][] {
+export function parseProgram_part1(str: string): [number, number][] {
   const regexp = /mul\(\d{1,3}\,\d{1,3}\)/gm;
   return str.match(regexp)?.map((mul) => {
     const [a, b] = mul.match(/\d{1,3}/g)!.map(Number);
@@ -9,7 +9,26 @@ export function parseProgram(str: string): [number, number][] {
   }) ?? [];
 }
 
-export function day03_part1(str: string): number {
-  const program = parseProgram(str);
-  return program.reduce((acc, [a, b]) => acc + a * b, 0);
+export function parseProgram_part2(str: string): [number, number][] {
+  const regexp = /do\(\)|don't\(\)|mul\(\d{1,3}\,\d{1,3}\)/gm;
+  return str.match(regexp)?.reduce<
+    { result: [number, number][]; lastInstruction: "do" | "don't" }
+  >((acc, mul) => {
+    if (mul === "do()") {
+      acc.lastInstruction = "do";
+    } else if (mul === "don't()") {
+      acc.lastInstruction = "don't";
+    } else {
+      const [a, b] = mul.match(/\d{1,3}/g)!.map(Number);
+      if (acc.lastInstruction === "do") {
+        acc.result.push([a, b]);
+      }
+    }
+
+    return acc;
+  }, { result: [], lastInstruction: "do" }).result ?? [];
+}
+
+export function runProgram(input: [number, number][]): number {
+  return input.reduce((acc, [a, b]) => acc + a * b, 0);
 }
--- days/03/main_test.ts
+++ days/03/main_test.ts
@@ -1,6 +1,6 @@
 import { expect } from "@std/expect";
 import { describe, test } from "@std/testing/bdd";
-import { day03_part1, parseProgram } from "./main.ts";
+import { parseProgram_part1, parseProgram_part2, runProgram } from "./main.ts";
 
 const txtExample = Deno.readTextFileSync(
   new URL("example.txt", import.meta.url),
@@ -11,8 +11,10 @@ const txtInput = Deno.readTextFileSync(
 );
 
 describe("Day 3: Mull It Over", () => {
-  test("parseProgram", () => {
-    expect(parseProgram(txtExample)).toEqual([
+  test("Part 1 • parseProgram", () => {
+    expect(parseProgram_part1("")).toEqual([]);
+
+    expect(parseProgram_part1(txtExample)).toEqual([
       [2, 4],
       [5, 5],
       [11, 8],
@@ -21,10 +23,31 @@ describe("Day 3: Mull It Over", () => {
   });
 
   test("Part 1 • Example", () => {
-    expect(day03_part1(txtExample)).toEqual(161);
+    const input = parseProgram_part1(txtExample);
+    expect(runProgram(input)).toEqual(161);
   });
 
   test("Part 1", () => {
-    expect(day03_part1(txtInput)).toEqual(189600467);
+    const input = parseProgram_part1(txtInput);
+    expect(runProgram(input)).toEqual(189600467);
+  });
+
+  test("Part 2 • parseProgram", () => {
+    expect(parseProgram_part2("")).toEqual([]);
+
+    expect(parseProgram_part2(txtExample)).toEqual([
+      [2, 4],
+      [8, 5],
+    ]);
+  });
+
+  test("Part 2 • Example", () => {
+    const input = parseProgram_part2(txtExample);
+    expect(runProgram(input)).toEqual(48);
+  });
+
+  test("Part 2", () => {
+    const input = parseProgram_part2(txtInput);
+    expect(runProgram(input)).toEqual(107069718);
   });
 });
