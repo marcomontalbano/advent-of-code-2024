0a3dd0423351e9cc32fc8dee8ec3a5911ebb69ad
Marco Montalbano <marcomontalbano.work@gmail.com>
2024-12-07 23:22:46 +0100

    feat: complete day 7 - part 1

--- days/07/main.ts
+++ days/07/main.ts
@@ -1,6 +1,70 @@
 // Day 7: Bridge Repair
 // https://adventofcode.com/2024/day/7
 
+export function parseProgram(
+  str: string,
+): Array<{ result: number; numbers: number[] }> {
+  return str.split("\n").map((line) => {
+    const [result, nums] = line.split(":");
+    return {
+      result: Number(result),
+      numbers: nums.trim().split(" ").map(Number),
+    };
+  });
+}
+
+type Signs = Array<"+" | "*">;
+
+export function matchResult(
+  input: ReturnType<typeof parseProgram>[number],
+): boolean {
+  const { result, numbers } = input;
+
+  function evaluateExpression(numbers: number[], signs: Signs): number {
+    return numbers.reduce((acc, num, index) => {
+      if (index === 0) return num;
+      switch (signs[index - 1]) {
+        case "+":
+          return acc + num;
+        case "*":
+          return acc * num;
+        default:
+          throw new Error("Invalid sign");
+      }
+    }, 0);
+  }
+
+  function generateSignCombinations(length: number): Signs[] {
+    const signs: Signs = ["+", "*"];
+    const combinations: Signs[] = [];
+    const totalCombinations = Math.pow(signs.length, length);
+
+    for (let i = 0; i < totalCombinations; i++) {
+      const combination: Signs = [];
+      let temp = i;
+      for (let j = 0; j < length; j++) {
+        const sign = temp % signs.length;
+        combination.push(signs[sign]);
+        temp = Math.floor(temp / signs.length);
+      }
+      combinations.push(combination);
+    }
+
+    return combinations;
+  }
+
+  return generateSignCombinations(numbers.length - 1).some(
+    (signCombination) =>
+      evaluateExpression(numbers, signCombination) === result,
+  );
+}
+
 export function runProgram_part1(str: string): number {
-  return 0
+  const program = parseProgram(str);
+
+  return program
+    .filter(matchResult)
+    .reduce((acc, cv) => {
+      return acc + cv.result;
+    }, 0);
 }
--- days/07/main_test.ts
+++ days/07/main_test.ts
@@ -1,13 +1,35 @@
 import { expect } from "@std/expect";
 import { describe, test } from "@std/testing/bdd";
-import { runProgram_part1 } from "./main.ts";
+import { parseProgram, runProgram_part1 } from "./main.ts";
 
 const txtExample = Deno.readTextFileSync(
   new URL("example.txt", import.meta.url),
 );
 
+const txtInput = Deno.readTextFileSync(
+  new URL("input.txt", import.meta.url),
+);
+
 describe("Day 7: Bridge Repair", () => {
   test("Part 1 • Example", () => {
+    expect(parseProgram(txtExample)).toEqual([
+      { result: 190, numbers: [10, 19] },
+      { result: 3267, numbers: [81, 40, 27] },
+      { result: 83, numbers: [17, 5] },
+      { result: 156, numbers: [15, 6] },
+      { result: 7290, numbers: [6, 8, 6, 15] },
+      { result: 161011, numbers: [16, 10, 13] },
+      { result: 192, numbers: [17, 8, 14] },
+      { result: 21037, numbers: [9, 7, 18, 13] },
+      { result: 292, numbers: [11, 6, 16, 20] },
+    ]);
+  });
+
+  test("Part 1 • Example", () => {
     expect(runProgram_part1(txtExample)).toEqual(3749);
+  });
+
+  test("Part 1", () => {
+    expect(runProgram_part1(txtInput)).toEqual(20665830408335);
   });
 });
