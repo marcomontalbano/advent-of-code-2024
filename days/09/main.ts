076295bb25c8ab357ae61b4ddd339ef0864faec4
Marco Montalbano <marcomontalbano.work@gmail.com>
2024-12-10 17:45:13 +0100

    feat: complete day 9 - part 1

--- days/09/main.ts
+++ days/09/main.ts
@@ -1,22 +1,62 @@
 // Day 9: Disk Fragmenter
 // https://adventofcode.com/2024/day/9
 
+type DiskMap = number[];
+type Disk = (string | number)[];
+
 export function parseInput(str: string): number[] {
   return str.split("").map(Number);
 }
 
-export function toDenseFormat(diskMap: number[]): string {
-  let result = "";
-  let fileId = 0;
+export function newArray<T extends unknown>(
+  length: number | undefined,
+  fill: T,
+): T[] {
+  if (length === undefined) {
+    return [];
+  }
 
-  diskMap.forEach((length, i) => {
-    if (i % 2 === 0) {
-      result += String(fileId).repeat(length);
-      fileId++;
-    } else {
-      result += ".".repeat(length);
-    }
-  });
+  return new Array(length).fill(fill);
+}
+
+export function toDenseFormat(diskMap: DiskMap): Disk {
+  const result: Disk = [];
+
+  for (let fileId = 0; fileId < diskMap.length / 2; fileId++) {
+    const size = diskMap[fileId * 2];
+    const space = diskMap[fileId * 2 + 1];
+    result.push(...newArray(size, fileId));
+    result.push(...newArray(space, "."));
+  }
 
   return result;
+}
+
+export function compact(disk: Disk): Disk {
+  for (let rightmostPos = disk.length - 1; rightmostPos > 0; rightmostPos--) {
+    if (disk[rightmostPos] === ".") {
+      continue;
+    }
+    const leftmostPos = disk.indexOf(".");
+    if (leftmostPos < rightmostPos) {
+      disk[leftmostPos] = disk[rightmostPos];
+      disk[rightmostPos] = ".";
+    }
+  }
+
+  return disk;
+}
+
+export function runProgram_part1(str: string): number {
+  const numbers = parseInput(str);
+  const initialMap = toDenseFormat(numbers);
+  const compacted = compact(initialMap);
+
+  return compacted.reduce((checksum: number, block, position) => {
+    if (block === ".") {
+      return checksum;
+    }
+
+    return checksum + (typeof block === "number" ? block * position : 0);
+  }, 0);
 }
--- days/09/main_test.ts
+++ days/09/main_test.ts
@@ -1,9 +1,19 @@
 import { expect } from "@std/expect";
 import { describe, test } from "@std/testing/bdd";
-import { parseInput, toDenseFormat } from "./main.ts";
+import {
+  newArray,
+  parseInput,
+  runProgram_part1,
+  toDenseFormat,
+} from "./main.ts";
+import { compact } from "./main.ts";
 
 const txtExample = Deno.readTextFileSync(
   new URL("example.txt", import.meta.url),
+);
+
+const txtInput = Deno.readTextFileSync(
+  new URL("input.txt", import.meta.url),
 );
 
 describe("Day 9: Disk Fragmenter", () => {
@@ -31,10 +41,126 @@ describe("Day 9: Disk Fragmenter", () => {
     ]);
   });
 
+  test("newArray", () => {
+    expect(newArray(5, 1)).toEqual([1, 1, 1, 1, 1]);
+    expect(newArray(3, ".")).toEqual([".", ".", "."]);
+    expect(newArray(undefined, ".")).toEqual([]);
+  });
+
   test("toDenseFormat", () => {
+    expect(toDenseFormat([1, 2, 3, 4, 5])).toEqual(
+      [0, ".", ".", 1, 1, 1, ".", ".", ".", ".", 2, 2, 2, 2, 2],
+    );
+
     const diskMap = parseInput(txtExample);
     expect(toDenseFormat(diskMap)).toEqual(
-      "00...111...2...333.44.5555.6666.777.888899",
+      [
+        0,
+        0,
+        ".",
+        ".",
+        ".",
+        1,
+        1,
+        1,
+        ".",
+        ".",
+        ".",
+        2,
+        ".",
+        ".",
+        ".",
+        3,
+        3,
+        3,
+        ".",
+        4,
+        4,
+        ".",
+        5,
+        5,
+        5,
+        5,
+        ".",
+        6,
+        6,
+        6,
+        6,
+        ".",
+        7,
+        7,
+        7,
+        ".",
+        8,
+        8,
+        8,
+        8,
+        9,
+        9,
+      ],
     );
+  });
+
+  test("compact", () => {
+    expect(compact(toDenseFormat([1, 2, 3, 4, 5]))).toEqual(
+      [0, 2, 2, 1, 1, 1, 2, 2, 2, ".", ".", ".", ".", ".", "."],
+    );
+
+    const diskMap = parseInput(txtExample);
+    const denseFormat = toDenseFormat(diskMap);
+    expect(compact(denseFormat)).toEqual(
+      [
+        0,
+        0,
+        9,
+        9,
+        8,
+        1,
+        1,
+        1,
+        8,
+        8,
+        8,
+        2,
+        7,
+        7,
+        7,
+        3,
+        3,
+        3,
+        6,
+        4,
+        4,
+        6,
+        5,
+        5,
+        5,
+        5,
+        6,
+        6,
+        ".",
+        ".",
+        ".",
+        ".",
+        ".",
+        ".",
+        ".",
+        ".",
+        ".",
+        ".",
+        ".",
+        ".",
+        ".",
+        ".",
+      ],
+    );
+  });
+
+  test("Part 1 • Example", () => {
+    expect(runProgram_part1(txtExample)).toEqual(1928);
+  });
+
+  test("Part 1", () => {
+    expect(runProgram_part1(txtInput)).toEqual(6337921897505);
   });
 });
